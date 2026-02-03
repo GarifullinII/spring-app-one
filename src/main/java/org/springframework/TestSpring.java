ea20747d86c8092d094fc07fbd69b966978c5c8e
GarifullinII <ildaridrisovich@gmail.com>
2026-02-03 21:09:20 +0400

    add scope singleton

--- src/main/java/org/springframework/TestSpring.java
+++ src/main/java/org/springframework/TestSpring.java
@@ -14,11 +14,18 @@ public class TestSpring {
         // Music music = context.getBean("musicBean", Music.class);
         // MusicPlayer musicPlayer = new MusicPlayer(music);
 
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
+        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        musicPlayer.playMusic();
 
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+        boolean comparison = firstMusicPlayer == secondMusicPlayer;
+
+        System.out.println(firstMusicPlayer.getName());
+        System.out.println(secondMusicPlayer.getVolume());
+        System.out.println(firstMusicPlayer);
+        System.out.println(secondMusicPlayer);
+        System.out.println(comparison);
+
 
         context.close();
     }
